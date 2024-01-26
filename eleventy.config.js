36f18bb35b300171c9c32777a7a7621c046587e8
Will Floyd <floyd.will@gmail.com>
2024-01-26 09:26:04 -0800

    remove eleventy-image plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,37 +1,8 @@
-const Image = require("@11ty/eleventy-img");
 const eleventyAutoCacheBuster = require("eleventy-auto-cache-buster");
-
-const imageShortcodeCallback =
-  async function(src, alt = '') {
-		const metadata = await Image(src, {
-			widths: [400, 800, 'auto'],
-			formats: ['jpeg'],
-      outputDir: 'dist/img',
-		});
-
-    const srcset =
-      metadata.jpeg.map(data => data.srcset).join(', ');
-
-		const lowsrc = metadata.jpeg[0];
-		const originalsrc = metadata.jpeg[metadata.jpeg.length - 1];
-
-    return `
-      <img
-        src="${lowsrc.url}"
-        srcset="${srcset}"
-        width="${originalsrc.width}"
-        height="${originalsrc.height}"
-        alt="${alt}"
-        loading="lazy"
-        decoding="async"
-      >
-    `;
-	};
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(eleventyAutoCacheBuster);
 
-	eleventyConfig.addShortcode('image', imageShortcodeCallback);
   eleventyConfig.addPassthroughCopy('src/assets');
 
   return {
